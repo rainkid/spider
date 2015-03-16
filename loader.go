3c98173f54787974bccfb70167dfde61eddd1003
sam <rainkide@gmail.com>
2015-03-16 14:11:59 +0800

    updategit

--- loader.go
+++ loader.go
@@ -111,7 +111,7 @@ func (l *Loader) GetRequest() {
 func (l *Loader) Send(data url.Values) ([]byte, error) {
 	l.data = data
 	transport := &http.Transport{
-    	ResponseHeaderTimeout:time.Duration(40 * time.Second),
+    	ResponseHeaderTimeout:time.Duration(60 * time.Second),
 		TLSClientConfig: &tls.Config{MaxVersion: tls.VersionTLS10, InsecureSkipVerify: true},
 	}
 
